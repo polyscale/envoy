598d963f58d0c9ac942047da9b08319e4cd0be65
benjamg <ben@benjamg.com>
2022-09-02 15:20:49 +0700

    Added connection data access to GenericUpstream, only supports TCP ones

--- source/common/tcp_proxy/upstream.h
+++ source/common/tcp_proxy/upstream.h
@@ -113,6 +113,7 @@ public:
   void encodeData(Buffer::Instance& data, bool end_stream) override;
   void addBytesSentCallback(Network::Connection::BytesSentCb cb) override;
   Tcp::ConnectionPool::ConnectionData* onDownstreamEvent(Network::ConnectionEvent event) override;
+  Tcp::ConnectionPool::ConnectionData* connectionData() override { return upstream_conn_data_.get(); };
 
 private:
   Tcp::ConnectionPool::ConnectionDataPtr upstream_conn_data_;
@@ -135,6 +136,7 @@ public:
   void encodeData(Buffer::Instance& data, bool end_stream) override;
   void addBytesSentCallback(Network::Connection::BytesSentCb cb) override;
   Tcp::ConnectionPool::ConnectionData* onDownstreamEvent(Network::ConnectionEvent event) override;
+  Tcp::ConnectionPool::ConnectionData* connectionData() override { return NULL; };
 
   // Http::StreamCallbacks
   void onResetStream(Http::StreamResetReason reason,
